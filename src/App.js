f7dabe8a6a5aa5236a219dcc35f91715cfb8539f
rpangarego <rpangarego@gmail.com>
2020-10-24 11:21:36 +0700

    initialize firebase and create connection

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import TinderCards from "./components/TinderCards";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            {/* Tinder card */}
+            <TinderCards />
+            {/* Buttons below tinder card */}
+          </Route>
+          <Route path="/chat">
+            <h1>Chat-page!</h1>
+            {/* Chats screen */}
+          </Route>
+          <Route path="/chat">
+            <h1>Chat-page!</h1>
+            {/* Chats screen */}
+          </Route>
+
+          {/* Individual chat screen */}
+        </Switch>
+      </Router>
     </div>
   );
 }
